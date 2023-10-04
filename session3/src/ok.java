a69e5c38d0b93abf99d44de8c06607c058f0c94b
thinhdh <daohuuthinh010415@gmail.com>
2023-10-04 08:36:11 +0700

    bt

---
+++ session3/src/ok.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ok {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] a = {"ok","aa","bb","cc"};
+        Arrays.sort(a);
+        System.out.println( Arrays.toString(a) );
+    }
+
+
+}
